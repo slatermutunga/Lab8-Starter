75df9019389f209da3231d2fffb2c790ad0cf4e3
Slater Mutunga <mutungaslater@gmail.com>
2024-05-29 12:54:55 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab8-Starter
+Name: Slater Mutunga
+
+How are graceful degradation and service workers related?
+Graceful degradtion and service workers are related as they are both used to enhance the user experience. Graceful degradation
+makes sure that the site is usuable on all browsers, and service workers allow websites help with faster load times and offline
+work. They are also related in that some browsers can't use service workers, so graceful degradation would account for that
+and still make sure that the service is functional.
--- sw.js
+++ sw.js
@@ -34,7 +34,16 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
